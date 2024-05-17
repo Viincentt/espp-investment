abe8424827df3e726dc33863c2889e2bd9d59906
Viincentt <vincent.hu@epita.fr>
2024-05-17 10:41:21 +0100

    update help message; factorize TP/SL

--- getPrices.py
+++ getPrices.py
@@ -19,19 +19,29 @@ def main():
     parser = ArgumentParser()
     SL, TP, price, limit = "SL", "TP", "price", "limit"
     parser.add_argument(price, help="Current price of stock")
-    parser.add_argument(TP, help="Take Profit percentage")
-    parser.add_argument(SL, help="Stop Loss percentage")
     parser.add_argument(
-        "-l", "--limit", dest=limit, help="Limit Order percentage", metavar="PERCENT"
+        TP,
+        help="Take Profit percentage. Use lowest historical profit - 1% to maximize chances to hit",
+    )
+    parser.add_argument(
+        SL,
+        help="Stop Loss percentage. Use highest historical loss + 1% to maximize space for errors",
+    )
+    parser.add_argument(
+        "-l",
+        "--limit",
+        dest=limit,
+        help="Limit Order percentage. Use lowest historical loss to maximixe chances order execute",
+        metavar="PERCENT",
     )
 
     args = parser.parse_args()
     SL, TP, price = float(args.SL), float(args.TP), float(args.price)
     if args.limit:
         price *= 1 - float(args.limit)
-    TP = price * (1 + TP)
-    SL = price * (1 - SL)
-    print("Buy at", price, "Take Profit at", round(TP, 2), "Stop Loss at", round(SL, 2))
+    TP = round(price * (1 + TP), 2)
+    SL = round(price * (1 - SL), 2)
+    print("Buy at", round(price, 2), "Take Profit at", TP, "Stop Loss at", SL)
 
 
 if __name__ == "__main__":
